7e31e5a5e00ac9e208c783fd7a191e902c452e84
Okurai <okurei99@gmail.com>
2024-09-04 15:40:37 +0200

    correzzione errori

--- src/DamageLogic.java
+++ src/DamageLogic.java
@@ -1,18 +1,4 @@
-/*Quando viene lanciato un attacco, il personaggio che lo riceve lancia un dado da 100 facce, se esce un numero:
-da 0 a 25 il colpo viene schivato e l'attacco non va a segno
-da 26 a 90, il colpo va a segno ma con una percentuale di danno che varia da 26% a 90%
-da 91 a 100 il colpo fa il 100% del danno.
-Sempre quando viene lanciato un attacco, il player che lo lancia deve lanciare due dadi da 6,
-        -  se il risultato è un doppio 1,2,3 il colpo è un FALLIMENTO (a prescindere dalla condizione realizzata con il lancio del dado del giocatore che riceve il colpo),
-- se invece esce un doppio 4,5,6 il colpo è CRITICO
--- in questo caso (COLPO CRITICO)  se IL RICEVENTE ha un numero compreso tra 26 e 90 avrà un danno che va dal 26 %al 90 %
-        , se il RICEVENTE, ha ottenuto un valore superiore a 90 il danno sarà del 100%
-I colpi dovranno essere lanciati in modo alternato.*/
-
-import java.util.Random;
-
 public class DamageLogic {
-    Random random = new Random();
 
 
     private double damage(double hit){
@@ -28,7 +14,7 @@ public class DamageLogic {
                 return dmg;
             }
             else{
-                dmg = (double)hit / 10;
+                dmg = hit / 10;
                 return dmg;
             }
         }
